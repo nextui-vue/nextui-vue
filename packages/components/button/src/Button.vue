6107bab2cc183fe5814eb1a774715e288a6162bb
hotdogc1017 <hotdogc1017@gmail.com>
2025-08-11 10:36:43 +0800

    refactor: complete migration of the Button component

--- packages/components/button/src/Button.vue
+++ packages/components/button/src/Button.vue
@@ -1,72 +1,23 @@
-<script lang="ts">
-import type { ButtonProps as Props } from "./use-button";
-import { type HTMLNextUIVueProps } from "@vue-nextui/shared";
-
-export interface ButtonProps extends HTMLNextUIVueProps<"button"> {
-  isLoading?: boolean;
-  disableRipple?: boolean;
-  spinnerPlacement?: "start" | "end";
-
-  /* Button variants */
-  variant?: Props["variant"];
-  size?: Props["size"];
-  color?: Props["color"];
-  radius?: Props["radius"];
-  fullWidth?: Props["fullWidth"];
-  isDisabled?: Props["isDisabled"];
-  isIconOnly?: Props["isIconOnly"];
-  disableAnimation?: Props["disableAnimation"];
-
-  /* Button aria */
-  role?: Props["role"]; // unsupported, just placeholder
-}
-
-type ButtonState = "pressed" | "hover";
-</script>
-
 <script lang="ts" setup>
+import { useButton, type ButtonProps } from "./use-button";
+
 import { computed, ref, type Component } from "vue";
-import { button } from "@heroui/theme";
-import { useRipple } from "@vue-nextui/shared";
 
-const props = withDefaults(defineProps<ButtonProps>(), {
-  disableRipple: false,
-  disableAnimation: false,
-  spinnerPlacement: "start",
-  color: "primary",
-});
+const props = withDefaults(defineProps<ButtonProps>(), {});
 
-const slots = defineSlots<{
+defineSlots<{
   default(): Component;
   startContent(): Component;
   endContent(): Component;
   spinner(): Component;
 }>();
 
-const buttonState = ref<ButtonState | undefined>();
-const buttonRef = ref<HTMLButtonElement>();
-useRipple(buttonRef);
-
-const className = computed(() => {
-  return button({ ...props });
-});
-
-function setButtonState(state?: ButtonState) {
-  buttonState.value = state;
-}
+const buttonRef = ref<HTMLButtonElement | null>();
+const bind = useButton(props, buttonRef);
 </script>
 
 <template>
-  <button
-    ref="buttonRef"
-    :data-pressed="buttonState === 'pressed'"
-    :data-hover="buttonState === 'hover'"
-    :class="className"
-    @mouseup="setButtonState()"
-    @mousedown="setButtonState('pressed')"
-    @mouseenter="setButtonState('hover')"
-    @mouseleave="setButtonState()"
-  >
+  <button ref="buttonRef" v-bind="bind">
     <slot name="startContent" />
     <slot name="spinner" />
     <slot />
