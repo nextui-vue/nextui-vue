e5c4e9e03c8386a70b1b3c5ee119534367fdaccd
hotdogc1017 <hotdogc1017@gmail.com>
2025-08-20 10:42:29 +0800

    build: mark `@vue-nextui/*` to no-external

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -4,6 +4,7 @@ import vue from "unplugin-vue/rolldown";
 export default defineConfig({
   entry: "src/index.ts",
   plugins: [vue()],
+  noExternal: [/^@vue-nextui\//],
   external: ["vue"],
   dts: {
     vue: true,
