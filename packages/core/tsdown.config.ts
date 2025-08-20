cf516dad16dc9564731fc22e744f0554903a688c
hotdogc1017 <hotdogc1017@gmail.com>
2025-08-20 11:21:59 +0800

    build: remove the noExternal option in the tsdown config file

--- packages/core/tsdown.config.ts
+++ packages/core/tsdown.config.ts
@@ -4,7 +4,6 @@ import vue from "unplugin-vue/rolldown";
 export default defineConfig({
   entry: "src/index.ts",
   plugins: [vue()],
-  noExternal: [/^@vue-nextui\//],
   external: ["vue"],
   dts: {
     vue: true,
