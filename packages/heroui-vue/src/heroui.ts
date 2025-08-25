a18bbe17f82f9a45c32f91ee6fce4f29b19f3736
hotdogc1017 <hotdogc1017@gmail.com>
2025-08-25 17:38:22 +0800

    build: update output for supporting browser

--- packages/heroui-vue/src/heroui.ts
+++ packages/heroui-vue/src/heroui.ts
@@ -1,3 +1,7 @@
+export { heroui } from "@heroui/theme";
 import { heroui } from "@heroui/theme";
+
+// For browser
+window && (window.heroui = heroui);
 
 export default heroui() as any;
--- packages/heroui-vue/tsdown.config.ts
+++ packages/heroui-vue/tsdown.config.ts
@@ -1,13 +1,25 @@
 import { defineConfig } from "tsdown";
 import vue from "unplugin-vue/rolldown";
 
-export default defineConfig({
-  entry: ["src/index.ts", "src/heroui.ts"],
-  plugins: [vue()],
-  external: ["vue"],
-  dts: {
-    vue: true,
-    sourcemap: true,
-    resolve: true,
+export default defineConfig([
+  {
+    entry: ["src/index.ts", "src/heroui.ts"],
+    plugins: [vue()],
+    external: ["vue"],
+    minify: true,
+    dts: {
+      vue: true,
+      sourcemap: true,
+      resolve: true,
+    },
   },
-});
+  {
+    entry: {
+      heroui: "src/heroui.ts",
+    },
+    minify: false,
+    platform: "browser",
+    format: "iife",
+    outDir: "dist/browser",
+  },
+]);
